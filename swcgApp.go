cb2b801eacba355b67045fa4ca6828d4d6679ba4
David St-Hilaire <sthilaire.david@gmail.com>
2013-09-24 14:59:42 -0400

    Added missing fields in card view

--- swcgApp.go
+++ swcgApp.go
@@ -32,12 +32,75 @@ type CardView struct {
 	Ressources      int
 	ForceIcons      int
 	CardCombatIcons string
-	Abilities       string
+	AbilitiesHTML   template.HTML
 	Health          int
 	Quote           string
 	ObjectiveSets   string
 	Set             string
 	Number          int
+}
+
+func CombatIconsHTML(ci *swcg.CardCombatIcons) string {
+	if ci == nil {
+		return "N/A"
+	}
+	return fmt.Sprintf(
+		"Combat Damage: %d [%d], Tactics: %d [%d], BlastDamage: %d [%d]",
+		ci.CombatDamage[0], ci.CombatDamage[1],
+		ci.Tactics[0], ci.Tactics[1],
+		ci.BlastDamage[0], ci.BlastDamage[1])
+}
+
+func ObjectiveSetsHTML(sets *[]swcg.ObjectiveSet) string {
+	out := ""
+	size := len(*sets)
+	for i, set := range *sets {
+		out += fmt.Sprintf("%d (%d/6)", set.SetId, set.CardSetNumber)
+		if i < size -1 {
+			out += ", "
+		}
+	}
+	return out
+}
+
+func AbilitiesHTML(abilities *[]swcg.AbilityInterface) string {
+	out := ""
+	traits := make([]*swcg.CardTrait,0)
+	for _, a := range *abilities {
+		switch ability := a.(type) {
+		case *swcg.CardAbility:
+			out += "<div class=\"ability\">"
+			out += fmt.Sprintf("<span class=\"abilityName\">%s</span><span class=\"abilityDesc\">%s</span>",
+				swcg.AbilityNames[ability.Type], ability.Description)
+			out += "</div>"
+		case *swcg.SimpleKeyword:
+			out += "<div class=\"keyword\">"
+			out += fmt.Sprintf("<span class=\"keyword\">%s</span>", swcg.KeywordNames[ability.K])
+			out += "</div>"
+		case *swcg.ComplexKeyword:
+			out += "<div class=\"keyword\">"
+			out += fmt.Sprintf("<span class=\"keyword\">%s [%d]</span>", swcg.KeywordNames[ability.K], ability.V)
+			out += "</div>"
+		case *swcg.ProtectKeywordType:
+			out += "<div class=\"keyword\">"
+			out += fmt.Sprintf("<span class=\"keyword\">%s from %s</span>", swcg.KeywordNames[ability.K], ability.ProtectedTrait)
+			out += "</div>"
+		case *swcg.CardTrait:
+			traits = append(traits, ability)
+		default:
+			out += ""
+		}
+	}
+	traitsStr := "<div class=\"traits\">"
+	traitSize := len(traits)
+	for i, trait := range traits {
+		traitsStr += "<span class=\"trait\">"+swcg.TraitNames[trait.Trait]+"</span>"
+		if i < traitSize -1 {
+			traitsStr += ", "
+		}
+	}
+	traitsStr += "</div>"
+	return traitsStr + out
 }
 
 func CreateView(c *swcg.Card) *CardView {
@@ -49,11 +112,11 @@ func CreateView(c *swcg.Card) *CardView {
 	v.Cost 	     	  = c.Cost
 	v.Ressources 	  = c.Ressources
 	v.ForceIcons 	  = c.ForceIcons
-	v.CardCombatIcons = "TODO"
-	v.Abilities 	  = "TODO"
+	v.CardCombatIcons = CombatIconsHTML(c.CardCombatIcons)
+	v.AbilitiesHTML   = template.HTML(AbilitiesHTML(&c.Abilities))
 	v.Health    	  = c.Health
 	v.Quote     	  = c.Quote
-	v.ObjectiveSets   = "TODO"
+	v.ObjectiveSets   = ObjectiveSetsHTML(&c.ObjectiveSets)
 	v.Set    	  = swcg.SetNames[c.Set]
 	v.Number 	  = c.Number
 
